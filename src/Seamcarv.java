2bcf53c24c9abf52a5407caa6368b13c5a526204
yuvalailer <yuvalailer@gmail.com>
2017-04-15 17:17:40 +0300

    cut seams

--- src/Seamcarv.java
+++ src/Seamcarv.java
@@ -1,10 +1,9 @@
 import javax.imageio.ImageIO;
+import java.awt.Image;
+import java.awt.image.BufferedImage;
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.Comparator;
-
-
-import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
 
@@ -20,7 +19,7 @@ public class Seamcarv {
 	int newRows = Integer.parseInt(args[3]);
 	int type = Integer.parseInt(args[4]);
 	File OUTfile = new File(args[5]);
-	int[][][] SeamMap = null; // TODO 
+	SeamMap[] seammap = null; // TODO 
 	
 	//create image: 
 	BufferedImage INimg = ImageIO.read(INfile);
@@ -31,14 +30,14 @@ public class Seamcarv {
 	int oldRows = INimg.getHeight();
 	
 	// get energy matrix: 
-	int[][] energyMatrix = getEnergy(INimg,type);
+	float[][] energyMatrix = energyCal(INimg,type);
 	
 	// Calculate map:
-	calculateSeamMap(energyMatrix,SeamMap);
+	calculateSeamMap(energyMatrix,seammap);
 	
 	// remove seams: 
 	int size = oldColumns - newColumns; // TODO - vertical vs horizontal ? 
-	cutSeams(INimg,SeamMap,size,OUTimg);
+	cutSeams(INimg,seammap,size,OUTimg);
 	
 	// write back the new Image: 
 	
@@ -52,18 +51,18 @@ public class Seamcarv {
 	}
 
 
-	private static void calculateSeamMap(int[][] energyMatrix, int[][][] seamMap) {
+	private static void calculateSeamMap(float[][] energyMatrix, SeamMap[] seammap) {
 		// calculate seams dynamically:
 		for (int i = 0; i < energyMatrix.length; i++) { 	// Iterate over the map's bottom row.  
-			seamMap[i] = dynamicSeam(energyMatrix,i); 		// Calculate the seams in a dynamic form
+			seammap[i] = dynamicSeam(energyMatrix,i); 		// Calculate the seams in a dynamic form
 		}
 		// sort by energy: 
-		sortSeamsMap(seamMap);								// sort map by energy 
+		Arrays.sort(seammap);								// sort map by energy 
 	}
 	
 
-	private static int[][] dynamicSeam(int[][] energyMatrix, int i) {
-		int[][] ans = {null,null,null};
+	private static SeamMap dynamicSeam(float[][] energyMatrix, int i) {
+		SeamMap ans = null;
 		int totalColumnEnergy = 0;
 		int[] sortPath = null;
 		
@@ -71,30 +70,127 @@ public class Seamcarv {
 			totalColumnEnergy += energyMatrix[i][j]; 
 		}
 		
-		ans[0][0] = totalColumnEnergy; 				// Weight of the entire seam. 
-		ans[1][0] = i;		//TODO - change to top index, not bottom..	// index of the top row seam index. 
-		ans[2] = sortPath; 	// TODO - to be 0,1,-1  // the path of the shortest seam 
+		ans.value = totalColumnEnergy; 				// Weight of the entire seam. 
+		ans.index = i;		//TODO - change to top index, not bottom..	// index of the top row seam index. 
+		ans.way = sortPath; 	// TODO - to be 0,1,-1  // the path of the shortest seam 
 		return ans;
 	}
 
-	private static void sortSeamsMap(int[][][] seamMap) {
-		//TODO- sort
-	}
-
-	private static void cutSeams(BufferedImage iNimg, int[][][] seamMap, int size, BufferedImage oUTimg) {
-		int[][] matrix = new int[iNimg.getWidth()][iNimg.getHeight()]; // TODO - is this the right dimm order.. 
-
-		for(int i = 0; i < 2048; i++)
-		    for(int j = 0; j < 2048; j++)
-		        arr[i][j] = image.getRGB(i, j);
+	private static void cutSeams(BufferedImage iNimg, SeamMap[] seammap, int size, BufferedImage oUTimg) {
+		int width = iNimg.getWidth();
+		int hight = iNimg.getHeight();
+		int[][] matrix = new int[iNimg.getWidth() - size][iNimg.getHeight()]; // TODO - is this the right dimm order.. 
+		boolean edit = true;
+		for(int i = 0; i < hight; i++)
+		    for(int j = 0; j < width; j++){ //TODO - dimensions
+		        for (int j2 = 0; j2 < size; j2++) {
+		        	if(j == seammap[j2].index){ edit = false; }
+				}
+		    	if(edit){
+		        	matrix[i][j] = iNimg.getRGB(i, j);
+		        } else {
+		        	continue;
+		        }
+		    }
+			
+		oUTimg = 
 		
-		BufferedImage tmpImg = iNimg;
-		for (int i = 0; i < size; i++) {
-			for (int j = 0; j < seamMap.length; j++) {
-				//if(seamMap[j][1] != j){
-					
+		}
+	// 
+	public static float[][] energyCal(BufferedImage img, int type) {
+		float[][] energyArr = null;
+		int height = img.getHeight();
+		int width = img.getWidth();
+		int neighbors = 8;
+		if (type != 2) {
+			for (int i = 0; i < height; i++) {
+				for (int j = 0; j < width; j++) {
+					if ((i==0 && j==0) || (i==height-1 && j==width-1) || (i==0 && j==width-1) || (i==height-1 && j==0)) {
+						neighbors = 3;
+					}
+					else if (i==0 || i==height-1 || j==0 || j==width-1) {
+						neighbors = 5;
+					}
+					else neighbors = 8;
+					energyArr[i][j] = (RGBdiff(img, i, j) / neighbors);
+					if (type == 1) {
+						energyArr[i][j] += entropy(img, i, j);
+					}
 				}
 			}
 		}
+		return energyArr;
+	}
+
+	public static float RGBdiff(BufferedImage img, int i, int j) {
+		int RGB = img.getRGB(i, j);
+		int r = (RGB >> 16) & 0xff;
+		int g = (RGB >> 8) & 0xff;
+		int b = (RGB) & 0xff;
+		int r2 = 0, g2 = 0, b2 = 0, RGB2 = 0;
+		float sum = 0;
+		
+		for (int k = i - 1; k < i + 2; k++) {
+			if (k == -1) k++;
+			if (k > (img.getHeight()-1)) continue;
+			
+			for (int l = j - 1; l < j + 2; j++) {
+				if (l == -1) l++;
+				if (l > (img.getWidth()-1)) continue;
+					
+				RGB2 = img.getRGB(k, l);
+				r2 = (RGB2 >> 16) & 0xff;
+				g2 = (RGB2 >> 8) & 0xff;
+				b2 = (RGB2) & 0xff;
+				sum += ((Math.abs(r - r2) + Math.abs(g - g2) + Math.abs(b - b2)) / 3);
+			}
+		}
+		return sum;
+	}
+
+	public static float entropy(BufferedImage img, int i, int j) {
+		float sum = 0, p = 0, h = 0;
+		int RGB2, r2 = 0, g2 = 0, b2 = 0, grey2 = 0;
+		int RGB = img.getRGB(i, j);
+		int r = (RGB >> 16) & 0xff;
+		int g = (RGB >> 8) & 0xff;
+		int b = (RGB) & 0xff;
+		int grey = (r + g + b) / 3;
+		for (int k = i - 4; k < i + 5; k++) {
+			if (k < 0) k = 0;
+			if (k > (img.getHeight()-1)) break;
+			for (int l = j - 4; l < j + 5; j++) {
+				if (l < 0) l = 0;
+				if (l > (img.getWidth()-1)) break;
+				RGB2 = img.getRGB(k, l);
+				r2 = (RGB2 >> 16) & 0xff;
+				g2 = (RGB2 >> 8) & 0xff;
+				b2 = (RGB2) & 0xff;
+				grey2 = (r2 + g2 + b2) / 3;
+				sum += grey;
+
+			}
+		}
+		for (int k = i - 4; k < i + 5; k++) {
+			if (k < 0) k = 0;
+			if (k > (img.getHeight()-1)) break;
+			for (int l = j - 4; l < j + 5; j++) {
+				if (l < 0) l = 0;
+				if (l > (img.getWidth()-1)) break;
+				RGB2 = img.getRGB(k, l);
+				r2 = (RGB2 >> 16) & 0xff;
+				g2 = (RGB2 >> 8) & 0xff;
+				b2 = (RGB2) & 0xff;
+				grey2 = (r2 + g2 + b2) / 3;
+				p = grey2 / sum;
+				h += p * Math.log(p);
+			}
+		}
+		h = -h;
+		return h;
+	}
+
+	
+	
 	
 }
